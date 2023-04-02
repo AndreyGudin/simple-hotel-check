277443fcca19d54d9ed2c10f2f7b21ecdde5ad28
Andrey Gudin <aeon150791@gmail.com>
2023-04-02 16:03:46 +0400

    init: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    semi: 0,
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/naming-convention': 'warn',
+    '@typescript-eslint/no-confusing-void-expression': 0
+  }
+};
