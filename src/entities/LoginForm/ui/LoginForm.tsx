7aaaf2788d6f88603a3ac0fecfc8ec190eddf718
Andrey Gudin <aeon150791@gmail.com>
2023-04-02 18:18:59 +0400

    feat: add react hool form, style for inputs

--- src/entities/LoginForm/ui/LoginForm.tsx
+++ src/entities/LoginForm/ui/LoginForm.tsx
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { useForm } from 'react-hook-form';
+
 import type { FC } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+
 import { Button } from '../../../shared/ui/Button/Button';
 import { Input } from '../../../shared/ui/Input/Input';
 
@@ -6,14 +12,30 @@ interface LoginFormProps {
   className?: string;
 }
 
+interface UserData {
+  login: string;
+  password: string;
+}
+
 export const LoginForm: FC<LoginFormProps> = ({
   className = ''
 }: LoginFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<UserData>({ mode: 'onChange' });
+
+  const onSubmit: SubmitHandler<UserData> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className={`${className}`}>
-      <Input title="Логин" name="user" />
-      <Input title="Пароль" type={'password'} name="password" />
-      <Button>Войти</Button>
-    </div>
+    <form className={`${className}`} onSubmit={handleSubmit(onSubmit)}>
+      <Input title="Логин" {...register('login')} />
+      <Input title="Пароль" {...register('password', { required: true })} />
+      {errors.password && <span>This field is required</span>}
+      <Button type="submit">Войти</Button>
+    </form>
   );
 };
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,7 +1,8 @@
-import type { FC, InputHTMLAttributes } from 'react';
+import type { FC, InputHTMLAttributes, RefAttributes } from 'react';
+import React from 'react';
 
 export enum InputTheme {
-  PRIMARY = 'text-red-200'
+  PRIMARY = 'w-full border border-[##C9CACC] rounded'
 }
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -9,24 +10,15 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   title: string;
 }
 
-export const Input: FC<InputProps> = (props: InputProps) => {
-  const {
-    theme = InputTheme.PRIMARY,
-    name,
-    type = 'text',
-    title,
-    ...otherProps
-  } = props;
-  return (
-    <div>
-      <label htmlFor={name}>{title}</label>
-      <input
-        type={type}
-        name={name}
-        id={name}
-        className={`${theme}`}
-        {...otherProps}
-      />
-    </div>
-  );
-};
+export const Input: FC<RefAttributes<HTMLInputElement> & InputProps> =
+  React.forwardRef<HTMLInputElement, InputProps>(function Input(
+    { theme = InputTheme.PRIMARY, title, ...otherProps },
+    ref
+  ) {
+    return (
+      <div className="flex flex-col gap-[7px]">
+        <label className="font-sans text-xs">{title}</label>
+        <input className={theme} {...otherProps} ref={ref} />
+      </div>
+    );
+  });
