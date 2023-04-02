18de001249bfffee447cfb077bad413ae6f741cf
Andrey Gudin <aeon150791@gmail.com>
2023-04-02 16:17:09 +0400

    feat: add empty login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,9 @@
+import { LoginPage } from '../pages/LoginPage';
+
 export default function App(): JSX.Element {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
 }
