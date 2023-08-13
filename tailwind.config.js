e10705992589ec54a63d09ac7cedaa9321506e32
Andrey Gudin <aeon150791@gmail.com>
2023-08-13 17:26:35 +0400

    fix: fix background image fordeploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'login-background': "url('./src/shared/assets/images/1.png')"
+      }
+    }
   },
   plugins: []
 };
